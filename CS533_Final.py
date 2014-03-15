d78ba455cf13eacc152613f4e39e5df60a7bf221
Austin Sharp <austinsharp111@gmail.com>
2014-03-14 17:35:50 -0700

    greedy policy added, and is part of experiment now.

--- CS533_Final.py
+++ CS533_Final.py
@@ -19,6 +19,64 @@ import game2
 def random_policy(game):
     return 0, random.choice(game.generate_moves())
 
+def _calculate_value(game, move):
+    ## game.player must match values on board.
+    ## using othello.directions for getting adjacent positions.
+
+    ## for each vector around the current position
+    player = game.player
+    totalValue = 0
+    for vector in othello.directions:
+        current_pos = move
+        print current_pos
+        adj_pos = (None,None)
+        vector_value = 0
+        playable = False
+        while True:
+            adj_pos = [current_pos[0] + vector[0], current_pos[1] + vector[1]]
+            print adj_pos
+            ## if adjacent position is outside board range exit inner loop
+            if adj_pos[0] >= othello.size or adj_pos[1] >= othello.size:
+                break
+            if adj_pos[0] < 0 or adj_pos[1] < 0:
+                break
+            ## if adjacent position is my own piece then exit inner loop
+            if game.board[adj_pos[0]][adj_pos[1]] == player:
+                playable = True
+                break
+            ## if adjacent position is open then
+            if game.board[adj_pos[0]][adj_pos[1]] == 0:
+                break
+            ## otherwise we know adj_space is opponent
+            ## continue moving in direction of vector add 1 to possible value
+            current_pos = adj_pos
+            vector_value += 1
+        if playable == True:
+            totalValue += vector_value
+    return totalValue
+
+
+def greedy_policy(game):
+    ## Test if no move available
+    if None in game.generate_moves():
+        return None
+    ## Otherwise there is at least 1 move to take.
+    else:
+        moves = game.generate_moves()
+        ## empty dictionary of (key:val = (move): int value of move)
+        moveDict = {key: None for key in moves}
+        for move in moves:
+            # TODO Figure out how to calculate the worth of different possible moves.
+            value = _calculate_value(game, move)
+            ## set dictionary value at specific key.
+            moveDict[move] = value
+            print moveDict
+        ## best key Move according to values.
+        bestMove = max(moveDict.iterkeys(), key=lambda x: moveDict[x])
+        return (0,bestMove)
+    ## Something stupid happened
+    raise RuntimeError("Something went wrong in Greedy Policy")
+
 
 def average(list):
     if len(list) == 0:
@@ -43,11 +101,14 @@ if __name__ == "__main__":
     #game2.play(othello.game(), game2.player(lambda x: minimax.minimax(x, 3)), game2.player(t.policy), True)
     #game2.play(othello.game(), game2.player(t.policy), game2.player(lambda x: minimax.minimax(x, 4)), True)
 
-    policies = [random_policy] # TODO add greedy
+    policies = {"random": random_policy, "greedy": greedy_policy}
     budgets = [1, 2, 5]
     c_vals = [1, 5, 10]#, 20, 50]
-    # TODO add greedy
-    opponents = {"random": game2.player(random_policy), "minimax2": game2.player(lambda x: minimax.minimax(x, 2))}#, game2.player(lambda x: minimax.minimax(x, 3)), game2.player(lambda x: minimax.minimax(x, 4))
+    opponents = {"random": game2.player(random_policy),
+                 "greedy": game2.player(greedy_policy),
+                 "minimax-2": game2.player(lambda x: minimax.minimax(x, 2)),
+                 "minimax-3": game2.player(lambda x: minimax.minimax(x, 3)),
+                 "minimax-4": game2.player(lambda x: minimax.minimax(x, 4))}
     # number of games going first/second with each configuration
     n = 10
 
@@ -57,22 +118,22 @@ if __name__ == "__main__":
             # for each opponent
                 # run n trials with us first, n trials with them first
 
-    for p in policies:
+    for pol_key in policies:
         for b in budgets:
             for c in c_vals:
-                for key in opponents:
-                    t = UCT_tree.Tree(b, p, c)
+                for opp_key in opponents:
+                    t = UCT_tree.Tree(b, policies[pol_key], c)
                     uct_player = game2.player(t.policy)
                     uct_black = []
                     uct_white = []
                     for i in range(n):
-                        b_result, b_game = game2.play(othello.game(), uct_player, opponents[key], False)
+                        b_result, b_game = game2.play(othello.game(), uct_player, opponents[opp_key], False)
                         uct_black.append(b_result)
                         #print b_game
-                        w_result, w_game = game2.play(othello.game(), opponents[key], uct_player, False)
+                        w_result, w_game = game2.play(othello.game(), opponents[opp_key], uct_player, False)
                         uct_white.append(w_result)
                         #print w_game
-                    print "Average score over " + str(n) + "trials for policy " + str(p) + ", budget " + str(b) + ", c = " + str(c) + ", opponent " + str(key) + ":"
+                    print "Average score over " + str(n) + "trials for default policy " + str(pol_key) + ", budget " + str(b) + ", c = " + str(c) + ", opponent " + str(opp_key) + ":"
                     print "Results as black (positive = we win):"
                     print str(average(uct_black))
                     print "Results as white (negative = we win):"
